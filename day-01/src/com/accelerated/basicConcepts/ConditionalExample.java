c07d9d88536c53581bb069977b5bf3cef09a4c25
Emre Aktaş <akt.emre@outlook.com>
2023-08-12 04:25:59 +0300

    Ternary Operator

--- day-01/src/com/accelerated/basicConcepts/ConditionalExample.java
+++ day-01/src/com/accelerated/basicConcepts/ConditionalExample.java
@@ -11,24 +11,37 @@ public class ConditionalExample {
         System.out.println(a > b);
         System.out.println(a < b);
 
+        System.out.println("-------------------------------");
+
         System.out.println("---If");
 
         String city = "ankara";
         int c = 15;
 
-        // And Conditional
+        // And Conditional &&
         if (city == "ankara" && c == 15) {
             System.out.println("Inside if section..");
-            //    if (c == 115) {
-            //        System.out.println("c : " + c);
-            //    }
-            // OR Conditional
-            //if (city == "ankara" || c==15) {
-            //        System.out.println("Inside if section..");
         } else if (city == "Ankara") {
             System.out.println("Inside other if section..");
         } else {
             System.out.println("Inside else section..");
         }
+        System.out.println("-------------------------------");
+        // OR Conditional ||
+        if (city == "Ankara" || c == 16) {
+            System.out.println("Inside if section..");
+        } else if (city == "Ankara") {
+            System.out.println("Inside other if section..");
+        } else {
+            System.out.println("Inside else section..");
+        }
+        System.out.println("-------------------------------");
+        // Ternary Operator ?:
+        int x = 69;
+        int y = 89;
+        int z = 79;
+
+        int largestNumber = (x > y) ? (x > z ? x : z) : (y > z ? y : z);
+        System.out.println("The largest numbers is:  " + largestNumber);
     }
 }
