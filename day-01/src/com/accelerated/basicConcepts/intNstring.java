2fc91a2a54999dbc5317963b8232916b8a3df6af
Emre Aktaş <akt.emre@outlook.com>
2023-08-12 06:25:51 +0300

    .

---
+++ day-01/src/com/accelerated/basicConcepts/intNstring.java
@@ -0,0 +1,23 @@
+package com.accelerated.basicConcepts;
+
+public class intNstring {
+
+    public static void main(String[] args) {
+
+        int a = (5 + 5) / 2;
+        double b = 5.0 / 2;
+
+        //b = 2.08;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+
+        String student = "Emre A.";
+        String courseCode = "YZL8419";
+        String courseName = "Mathematics";
+
+        System.out.print("\n1st line Course Code: " + courseCode);
+        System.out.print("\n2nd line Course Name: " + courseName);
+        System.out.print("\n3th line Student " + student);
+        //System.out.print("4.satir");
+    }
+}
