1e2e3ec17a8000a82db0532d3282e3e874407ce5
Emre Aktaş <akt.emre@outlook.com>
2023-08-14 00:33:30 +0300

    edit

--- day-01/src/com/accelerated/basicConcepts/StringConcatination.java
+++ day-01/src/com/accelerated/basicConcepts/StringConcatination.java
@@ -4,7 +4,7 @@ public class StringConcatination {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello " + " BilgeAdam");
+        System.out.println("Hello " + "BilgeAdam");
         System.out.println("Hello " + new Student());
     }
 
